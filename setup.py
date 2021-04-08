042d4a06062ffb46b0e99ad138debc1ac3e09bfd
Ville M. Vainio <ville.vainio@basware.com>
2021-04-08 08:10:23 +0300

    remove scripts

--- setup.py
+++ setup.py
@@ -1,16 +1,12 @@
 from distutils.core import setup
 
-setup(name='supermoto',
-      version='1.0.0',
-      description='Helpers for "moto" tests',
-      author='Ville M. Vainio',
-      author_email='ville.vainio@basware.com',
-      url='https://github.com/vivainio/supermoto',
-      packages=['supermoto'],
-      install_requires=[],
-      entry_points = {
-        'console_scripts': [
-            'supermoto = supermoto.supermoto:main'
-        ]
-      }
-     )
+setup(
+    name="supermoto",
+    version="1.0.0",
+    description='Helpers for "moto" tests',
+    author="Ville M. Vainio",
+    author_email="ville.vainio@basware.com",
+    url="https://github.com/vivainio/supermoto",
+    packages=["supermoto"],
+    install_requires=[],
+)
