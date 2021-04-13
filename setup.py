daa8c63a556c4e68d45a3dcd2d8cab921347fd63
Ville M. Vainio <ville.vainio@basware.com>
2021-04-13 14:31:33 +0300

    bump ver

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="supermoto",
-    version="1.2.1",
+    version="1.2.2",
     description='Helpers for "moto" tests',
     author="Ville M. Vainio",
     author_email="ville.vainio@basware.com",
